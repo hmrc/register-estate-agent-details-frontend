0709a094c9ac4000fefb84a7ace58d5a784dcc2b
Adam George <151747054+adageorg@users.noreply.github.com>
2024-07-18 15:17:36 +0100

    DDCE-5562: Upgrading to version 9 of play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoHmrcVersion = "1.9.0"
+  private val mongoHmrcVersion = "2.1.0"
   private val bootstrapVersion = "8.6.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoHmrcVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "8.5.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.11.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                        % "9.0.0"
@@ -17,9 +17,9 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoHmrcVersion,
     "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.18.0",
-    "org.jsoup"             % "jsoup"                   % "1.17.2",
+    "org.jsoup"             % "jsoup"                   % "1.18.1",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0",
-    "org.mockito"          %% "mockito-scala-scalatest" % "1.17.31"
+    "org.mockito"          %% "mockito-scala-scalatest" % "1.17.37"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]      = compile ++ test
