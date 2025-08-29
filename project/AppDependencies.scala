df63bf603a66cb1b4192d07514697535ee40ab8c
harish-korada1 <174133905+harish-korada1@users.noreply.github.com>
2025-08-29 08:08:19 +0100

    DDCE-7012:[Estates]: Fix Jackson Core Vulnerability [register-estate-agent-details-frontend] updating the PR comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,11 +4,11 @@ object AppDependencies {
 
   private val mongoHmrcVersion = "2.7.0"
 
-  private val bootstrapVersion = "9.18.0"
+  private val bootstrapVersion = "9.19.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoHmrcVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.0.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.8.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0"
