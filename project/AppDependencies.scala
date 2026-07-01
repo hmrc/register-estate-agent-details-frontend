d32b3295b1902e4da3da26e2ae0a46a8a6b23482
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2026-07-01 11:03:38 +0100

    DDCE-8576 play frontend update and clean up

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoHmrcVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.7.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"       %% "domain-play-30"                        % "11.0.0"
@@ -17,7 +17,7 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoHmrcVersion,
-    "org.scalatestplus"    %% "scalacheck-1-19"         % "3.2.19.0",
+    "org.scalatestplus"    %% "scalacheck-1-19"         % "3.2.20.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)
 
